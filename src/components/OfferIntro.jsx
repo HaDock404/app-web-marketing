6c72b6831d9a89067f32c65ee7f685be66a2ec36
HaDock404 <delescluse.g@gmail.com>
2024-11-27 10:18:00 +0100

    offer_intro

---
+++ src/components/OfferIntro.jsx
@@ -0,0 +1,11 @@
+import '../styles/large_page.css'
+
+function OfferIntro({text}) {
+    return (
+        <div className='intro_offer_page'>
+            {text}
+        </div>
+    )
+}
+
+export default OfferIntro
--- src/pages/WebEssential/WebEssentialLargePage.jsx
+++ src/pages/WebEssential/WebEssentialLargePage.jsx
@@ -1,12 +1,16 @@
 import '../../styles/large_page.css'
 
 import HeaderLargePage from "../../components/HeaderLargePage"
+import OfferTitle from '../../components/OfferTitle'
+import OfferIntro from '../../components/OfferIntro'
 
 function WebEssentialLargePage() {
     return (
         <section className="section_large_page">
             <article id="web_essential_large_page_el1">
                 <HeaderLargePage />
+                <OfferTitle title="WebEssential"/>
+                <OfferIntro text="L’offre parfaite pour débuter son activité sans prise de tête"/>
             </article>
             <article id="web_essential_large_page_el2">
             </article>
