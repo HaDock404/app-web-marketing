4532a6905279baf047ab699f8c8614e4087385e0
HaDock404 <delescluse.g@gmail.com>
2024-11-27 09:42:44 +0100

    WebEssential

--- src/pages/WebEssential/WebEssentialLargePage.jsx
+++ src/pages/WebEssential/WebEssentialLargePage.jsx
@@ -1,6 +1,16 @@
+import '../../styles/large_page.css'
+
+import HeaderLargePage from "../../components/HeaderLargePage"
+
 function WebEssentialLargePage() {
     return (
-        <div>large page</div>
+        <section className="section_large_page">
+            <article id="web_essential_large_page_el1">
+                <HeaderLargePage />
+            </article>
+            <article id="web_essential_large_page_el2">
+            </article>
+        </section>
     )
 }
 
