09d63a6ea9cc53b417d65ee15c6c828fe388d6f8
HaDock404 <delescluse.g@gmail.com>
2024-11-27 11:37:19 +0100

    offer_paragraph

--- src/pages/WebEssential/WebEssentialLargePage.jsx
+++ src/pages/WebEssential/WebEssentialLargePage.jsx
@@ -3,6 +3,7 @@ import '../../styles/large_page.css'
 import HeaderLargePage from "../../components/HeaderLargePage"
 import OfferTitle from '../../components/OfferTitle'
 import OfferIntro from '../../components/OfferIntro'
+import OfferParagraph from '../../components/OfferParagraph'
 
 function WebEssentialLargePage() {
     return (
@@ -11,6 +12,23 @@ function WebEssentialLargePage() {
                 <HeaderLargePage />
                 <OfferTitle title="WebEssential"/>
                 <OfferIntro text="L’offre parfaite pour débuter son activité sans prise de tête"/>
+                <OfferParagraph
+                    title='1. Présentation'
+                    content={`
+                        Avec WebEssentiel c’est <span class="span_underline">simple</span> :
+                        <ul>
+                            <li class="custom_bullet">
+                                Vous sélectionnez le template qui vous correspond le mieux. 
+                            </li>
+                            <li class="custom_bullet">
+                                Vous nous contactez en nous donnant les informations qui apparaitront sur votre site
+                            </li>
+                            <li class="custom_bullet">
+                                En <span class="span_underline">48h</span> votre site est en ligne et vos <span class="span_underline">clients</span> commencent à vous contacter
+                            </li>
+                        </ul>
+                        `}
+                />
             </article>
             <article id="web_essential_large_page_el2">
             </article>
