e97399b53704067f2a5e730ca577afb993860a7f
HaDock404 <delescluse.g@gmail.com>
2024-12-01 17:53:21 +0100

    icons

---
+++ src/components/SmartphoneIcon.jsx
@@ -0,0 +1,20 @@
+function SmartphoneIcon() {
+    return (
+        <svg className="smartphoneIcon" width="auto" height="100%" viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_94_211)">
+            <rect x="107" y="4" width="205" height="412" rx="36" stroke="white" stroke-width="8"/>
+            <rect x="99.5" y="85.5" width="5" height="30" fill="white" stroke="white"/>
+            <rect x="99.5" y="121.5" width="5" height="30" fill="white" stroke="white"/>
+            <rect x="314.5" y="85.5" width="5" height="30" fill="white" stroke="white"/>
+            <rect x="181.5" y="31.5" width="7" height="57" rx="3.5" transform="rotate(-90 181.5 31.5)" fill="white" stroke="white"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_94_211">
+            <rect width="420" height="420" fill="white" transform="matrix(0 1 1 0 0 0)"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export default SmartphoneIcon
