4e113abec65f5fe621486a9d9e8b2a44dc4aa45b
HaDock404 <delescluse.g@gmail.com>
2024-12-05 14:12:11 +0100

    message for small screen size

--- src/pages/HomeSmallPage.jsx
+++ src/pages/HomeSmallPage.jsx
@@ -1,8 +1,22 @@
 function HomeSmallPage() {
     return (
         <section className="page_section">
-            <article  id="small_page_el1">
+            <article 
+                id="small_page_el1"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '25px',
+                    fontWeight: 'bold',
+                    textDecoration: 'underline'
+                }}
 
+            >
+                Screen Size Not Supported Yet
             </article>
         </section>
     )
--- src/pages/OffersSmallPage.jsx
+++ src/pages/OffersSmallPage.jsx
@@ -1,6 +1,20 @@
 function OffersSmallPage() {
     return (
-        <div>Small page</div>
+        <section
+            style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '25px',
+                fontWeight: 'bold',
+                textDecoration: 'underline'
+            }}
+        >
+            Screen Size Not Supported Yet
+        </section>
     )
 }
 
