ccea1b6527a684484ad33f0a9d943d06a503571a
HaDock404 <delescluse.g@gmail.com>
2024-12-12 11:28:19 +0100

    test opacity

--- src/components/smallScreen/Message3.jsx
+++ src/components/smallScreen/Message3.jsx
@@ -11,15 +11,15 @@ function Message3() {
                     <div className="message3_number" style={{backgroundColor:'#ffff', color:'#6400E3'}}>1</div>
                     <div className="message3_text">Sélectionnez votre <strong className="message3_text_strong">activité</strong>.</div>
                 </li>
-                <li className="message3_li">
+                <li className="message3_li message_hidden">
                     <div className="message3_number" style={{backgroundColor:'#6400E3', color:'#ffff'}}>2</div>
                     <div className="message3_text">Choisissez un <strong className="message3_text_strong">modèle</strong>.</div>
                 </li>
-                <li className="message3_li">
+                <li className="message3_li message_hidden">
                     <div className="message3_number" style={{backgroundColor:'#6400E3', color:'#ffff'}}>3</div>
                     <div className="message3_text">Ajoutez vos <strong className="message3_text_strong">informations</strong>.</div>
                 </li>
-                <li className="message3_li">
+                <li className="message3_li message_hidden">
                     <div className="message3_number" style={{backgroundColor:'#6400E3', color:'#ffff'}}>4</div>
                     <div className="message3_text">Publiez en un <strong className="message3_text_strong">clic</strong>.</div>
                 </li>
