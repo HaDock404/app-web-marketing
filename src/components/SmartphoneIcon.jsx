ea14bff4cacc718ddfcf9305879908d78b2ec4e4
HaDock404 <delescluse.g@gmail.com>
2025-01-07 14:45:49 +0100

    bug with smartphone icon trying to fix in prod

--- src/components/SmartphoneIcon.jsx
+++ src/components/SmartphoneIcon.jsx
@@ -1,6 +1,6 @@
 function SmartphoneIcon({color}) {
     return (
-        <svg width="auto" height="100%" viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="auto" height="80%" viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_96_162)">
             <rect x="114.5" y="11.5" width="190" height="397" rx="28.5" stroke={color} strokeWidth="23"/>
             <rect x="316.5" y="85.5" width="6" height="30" fill={color} stroke={color}/>
