9c26dae97003a823dae45fa872ea1cfa8e46ac4d
HaDock404 <delescluse.g@gmail.com>
2025-01-08 15:53:45 +0100

    ConnexionPage working

--- src/pages/Connexion/ConnexionPage.jsx
+++ src/pages/Connexion/ConnexionPage.jsx
@@ -1,5 +1,27 @@
+import React, { useState, useEffect } from "react";
+import '../../styles/connexion.css'
+
+import ConnexionLargePage from './ConnexionLargePage'
+import ConnexionSmallPage from './ConnexionSmallPage'
+
 function ConnexionPage() {
-    return
+    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1024);
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth > 1024);
+        };
+    
+        window.addEventListener("resize", handleResize);
+    
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return (
+        <>{isLargeScreen ? <ConnexionLargePage /> : <ConnexionSmallPage />}</>
+    )
 }
 
 export default ConnexionPage
