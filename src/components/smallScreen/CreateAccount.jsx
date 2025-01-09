e9f9b3e06c65d0169ece1dd5d2dcfe6ef21cc946
HaDock404 <delescluse.g@gmail.com>
2025-01-09 21:52:25 +0100

    tierce party logo connexion

--- src/components/smallScreen/CreateAccount.jsx
+++ src/components/smallScreen/CreateAccount.jsx
@@ -1,6 +1,23 @@
+import FacebookLogo from './FacebookLogo.jsx'
+import GoogleLogo from './GoogleLogo.jsx'
+import InstaLogo from './InstaLogo.jsx'
+
 function CreateAccount() {
     return (
-        <div>hello</div>
+        <article className="create_account_article">
+            <h1 className="create_account_h1">Inscription rapide</h1>
+            <div className="create_account_tierce-party">
+                <button className="create_account_tierce-party_button">
+                    <GoogleLogo />
+                </button>
+                <button className="create_account_tierce-party_button">
+                    <FacebookLogo />
+                </button>
+                <button className="create_account_tierce-party_button" style={{padding:"0"}}>
+                    <InstaLogo />
+                </button>
+            </div>
+        </article>
     )
 }
 
