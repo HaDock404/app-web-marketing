200ba9342d9f77e609e201782d1472653467a23a
HaDock404 <delescluse.g@gmail.com>
2025-01-22 09:17:34 +0100

    dashboard box params

--- src/components/smallScreen/DashboardBoxParams.jsx
+++ src/components/smallScreen/DashboardBoxParams.jsx
@@ -10,12 +10,42 @@ function DashboardBoxParams() {
         <article className="dashboard_box_params_article">
             <div className="dashboard_box_params_title">Gestion de votre compte</div>
             <ul className='dashboard_box_params_ul'>
-                <li>Parrainer un ami</li>
-                <li>Modifier la carte de crédit</li>
-                <li>Abonnement</li>
-                <li>E-mail & Mot de passe</li>
-                <li>Factures</li>
-                <li>Code de Promotion</li>
+                <li className="dashboard_box_params_li">
+                    <div className="dashboard_box_params_svg">
+                        <GiftIcon />
+                    </div>
+                    Parrainer un ami
+                </li>
+                <li className="dashboard_box_params_li">
+                    <div className="dashboard_box_params_svg">
+                        <CardIcon />
+                    </div>
+                    Modifier la carte de crédit
+                </li>
+                <li className="dashboard_box_params_li">
+                    <div className="dashboard_box_params_svg">
+                        <SubIcon />
+                    </div>
+                    Abonnement
+                </li>
+                <li className="dashboard_box_params_li">
+                    <div className="dashboard_box_params_svg">
+                        <MailIcon />
+                    </div>
+                    E-mail & Mot de passe
+                </li>
+                <li className="dashboard_box_params_li">
+                    <div className="dashboard_box_params_svg">
+                        <BillIcon />
+                    </div>
+                    Factures
+                </li>
+                <li className="dashboard_box_params_li">
+                    <div className="dashboard_box_params_svg">
+                        <CodeIcon />
+                    </div>
+                    Code & Promotion
+                </li>
             </ul>
         </article>
     )
--- src/components/smallScreen/MailIcon.jsx
+++ src/components/smallScreen/MailIcon.jsx
@@ -1,6 +1,6 @@
 function MailIcon() {
     return (
-        <svg style={{width:'auto', height:'100%'}} width="76" height="48" viewBox="0 0 76 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg style={{width:'100%', height:'auto'}} width="76" height="48" viewBox="0 0 76 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="3" y="3" width="70" height="42" rx="6" stroke="#6400E3" stroke-width="6"/>
             <rect x="2" y="8.34375" width="6" height="42.4389" rx="3" transform="rotate(-62.9547 2 8.34375)" fill="#6400E3"/>
             <rect width="6" height="42.484" rx="3" transform="matrix(-0.454695 -0.890647 -0.890647 0.454695 75.1504 8.34375)" fill="#6400E3"/>
